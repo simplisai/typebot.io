2c2fcb1ba1ba033005c9cfcff407665d9a4ff271
Simplis.ai <gestaosimplisai@gmail.com>
2025-09-02 13:31:23 -0300

    Delete apps/landing-page/src directory

--- apps/landing-page/src/helpers/setCookie.ts
+++
@@ -1,7 +0,0 @@
-import { serializeTypebotCookie } from "@typebot.io/telemetry/cookies/helpers";
-
-export const setCookie = (consent: "declined" | "accepted") => {
-  document.cookie = serializeTypebotCookie({
-    consent,
-  });
-};
